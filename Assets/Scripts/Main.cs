c1a41977b8c494c786dba4213a043e55dd92e341
omarcosvbn <omarcosvbn@gmail.com>
2024-04-03 14:27:49 +0100

    test

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour{
 
-
+    GameObject textMoney = GameObject.Find("Money");
+    
+    float money = 0;
     void Update(){
-        
+        textMoney.text = "a";
     }
 }
