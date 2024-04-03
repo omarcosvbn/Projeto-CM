d00b92422dc61ac5d5d20e74b9d059508f15065d
omarcosvbn <omarcosvbn@gmail.com>
2024-04-03 15:40:46 +0100

    texts

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using System;
+using System.Security.Cryptography.X509Certificates;
 
 public class NewBehaviourScript : MonoBehaviour{
 
-    GameObject textMoney = GameObject.Find("Money");
-    
-    float money = 0;
+    public TextMeshProUGUI textMoney;
+    //PublicKey Button upgrade1;
+    float money = 0f;
+    float moneyPerSecond = 1f;
+    float upgrades = 1f;
     void Update(){
-        textMoney.text = "a";
+        money += (moneyPerSecond * upgrades) * Time.deltaTime;
+        textMoney.text = money.ToString("F2");
     }
 }
