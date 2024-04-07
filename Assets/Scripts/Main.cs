eca98ef014d69dca36da809fec2c64653a35055f
omarcosvbn <omarcosvbn@gmail.com>
2024-04-07 03:39:22 +0100

    fim da aula

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
 using System;
 using System.Security.Cryptography.X509Certificates;
+
 
 public class NewBehaviourScript : MonoBehaviour{
 
     public TextMeshProUGUI textMoney;
-    //PublicKey Button upgrade1;
+    public Image upgrade1;
+
+    
     float money = 0f;
     float moneyPerSecond = 1f;
     float upgrades = 1f;
