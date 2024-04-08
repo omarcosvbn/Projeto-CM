ad1335bd4bf1f6fecd17f39bc7aadffa630f6e4b
omarcosvbn <omarcosvbn@gmail.com>
2024-04-09 00:45:34 +0100

    upgrade button

--- Assets/Scripts/WonkasFactory.cs
+++ Assets/Scripts/WonkasFactory.cs
@@ -6,16 +6,19 @@ public class WonkasFactory : MonoBehaviour{
 
     public Text moneyText;
     public double money;
+    public float upgrades;
 
     public void Start(){
         money = 0;
-
+        upgrades = 1;
     }
     
     public void Update(){
-        money += 1 * Time.deltaTime;
+        money += 1 * upgrades * Time.deltaTime;
         moneyText.text = "Money: " + money.ToString("F2");
     }
 
-    //public void Upgrade
+    public void Upgrade(){
+        upgrades *= 1.5f;
+    }
 }
