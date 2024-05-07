14fdf0846eb193e57ed469412675ca70fdf8fd60
ClaraAriGomes <clararigomes@gmail.com>
2024-05-07 19:45:59 +0100

    precos adicionados na descricao

--- Assets/Scripts/WonkasFactory.cs
+++ Assets/Scripts/WonkasFactory.cs
@@ -10,35 +10,35 @@ public class WonkasFactory : MonoBehaviour{
     // DEFINE LIST WITH UPGRADES
     Upgrade[] _Upgrades = new Upgrade[]{
         //Chocolate upgrades
-        new Upgrade {Name = "More cacao pods", Description = "You invest in transportation to get more cacao pods to your factory.", Boost= "Chocolate production +50%", Cost="1$", Order = 1},
-        new Upgrade {Name = "Bigger roasters", Description = "More chocolate can be roasted simultaneously.", Boost= "Chocolate production +50%", Cost="1$", Order = 2},
-        new Upgrade {Name = "Chocolate Tempering Machine", Description = "Install advanced machines for faster and more precise chocolate tempering, resulting in better quality chocolate", Boost= "Chocolate production +200%", Cost="1$", Order = 3},
-        new Upgrade {Name = "Extra Sugar", Description = "People became more addicted to your chocolate", Boost= "Sales +50%", Cost="1$", Order = 4},
-        new Upgrade {Name = "Oompa Loompas", Description = "You take a trip to Loompaland and invite its inhabitants to work at your factory in exchange for cocoa beans.", Boost= "Unlocks Oompa Loompas", Cost="1$", Order = 5},
-        new Upgrade {Name = "Chocolate Tasting Room", Description = "Create a luxurious tasting room where visitors can sample a variety of chocolate creations.", Boost= "Sales +25%", Cost="1$", Order = 6},
+        new Upgrade {Name = "More cacao pods", Description = "You invest in transportation to get more cacao pods to your factory.", Boost= "Chocolate production +50%", Cost="50$", Order = 1},
+        new Upgrade {Name = "Bigger roasters", Description = "More chocolate can be roasted simultaneously.", Boost= "Chocolate production +50%", Cost="450$", Order = 2},
+        new Upgrade {Name = "Chocolate Tempering Machine", Description = "Install advanced machines for faster and more precise chocolate tempering, resulting in better quality chocolate", Boost= "Chocolate production +200%", Cost="200$", Order = 3},
+        new Upgrade {Name = "Extra Sugar", Description = "People became more addicted to your chocolate", Boost= "Sales +50%", Cost="100$", Order = 4},
+        new Upgrade {Name = "Oompa Loompas", Description = "You take a trip to Loompaland and invite its inhabitants to work at your factory in exchange for cocoa beans.", Boost= "Unlocks Oompa Loompas", Cost="300$", Order = 5},
+        new Upgrade {Name = "Chocolate Tasting Room", Description = "Create a luxurious tasting room where visitors can sample a variety of chocolate creations.", Boost= "Sales +25%", Cost="1000$", Order = 6},
        
         //Bubblegum upgrades
-        new Upgrade {Name = "Bubblegum", Description = "You want create more than just chocolate. How about bubblegum?", Boost= "New factory sector", Cost="1$", Order = 7},
-        new Upgrade {Name = "New flavor - Breakfast", Description = "You have a ground-breaking idea. A meal flavored Bubblegum.", Boost= "Bubblegum production +33.33%", Cost="1$", Order = 8},
-        new Upgrade {Name = "New flavor - Lunch", Description = "Take your previous idea to the next level by creating another meal based flavor.", Boost= "Bubblegum production +33.33%", Cost="1$", Order = 9},
-        new Upgrade {Name = "New flavor - Dinner", Description = "Since you have created breakfast and lunch flavored gum, might as well finish it off with a dinner flavored one", Boost= "Bubblegum production +33.33%", Cost="1$", Order = 10},
-        new Upgrade {Name = "OompaLoompa Housing", Description = "With a new place to rest, the Oompa Loompas become more productive", Boost= "New factory sector with faster production", Cost="1$", Order = 11},
-        new Upgrade {Name = "3-Course's Dinner Gum", Description = "A final, more sophisticated version of the dinner flavor. WARNING: unexpected side effects may occur upon consumption", Boost= "Bubblegum production +100%; Sales +200%", Cost="1$", Order = 12},
+        new Upgrade {Name = "Bubblegum", Description = "You want create more than just chocolate. How about bubblegum?", Boost= "New factory sector", Cost="20000$", Order = 7},
+        new Upgrade {Name = "New flavor - Breakfast", Description = "You have a ground-breaking idea. A meal flavored Bubblegum.", Boost= "Bubblegum production +33.33%", Cost="500$", Order = 8},
+        new Upgrade {Name = "New flavor - Lunch", Description = "Take your previous idea to the next level by creating another meal based flavor.", Boost= "Bubblegum production +33.33%", Cost="600$", Order = 9},
+        new Upgrade {Name = "New flavor - Dinner", Description = "Since you have created breakfast and lunch flavored gum, might as well finish it off with a dinner flavored one", Boost= "Bubblegum production +33.33%", Cost="700$", Order = 10},
+        new Upgrade {Name = "OompaLoompa Housing", Description = "With a new place to rest, the Oompa Loompas become more productive", Boost= "New factory sector with faster production", Cost="50000$", Order = 11},
+        new Upgrade {Name = "3-Course's Dinner Gum", Description = "A final, more sophisticated version of the dinner flavor. WARNING: unexpected side effects may occur upon consumption", Boost= "Bubblegum production +100%; Sales +200%", Cost="900$", Order = 12},
         
         //Gobstopper upgrades
-        new Upgrade {Name = "Gobstoppers", Description = "You need more variety of sweets to appeal to more people. How about something that lasts in your mouth but you do not need to chew?", Boost= "New factory sector", Cost="1$", Order = 13},
-        new Upgrade {Name = "New flavors", Description = "You create new flavors of Godstoppers", Boost= "Sales +50%", Cost="1$", Order = 14},
-        new Upgrade {Name = "A variety of flavors, One Gobstopper", Description = "What if you combine all of the flavors created into one single gobstopper?", Boost= "Gobstopper Production +50%", Cost="1$", Order = 15},
-        new Upgrade {Name = "New recipe - Longer lasting", Description = "People like your Gobstoppers so much they are asking for longer lasting ones! Create a new recipe for them to last longer.", Boost= "Gobstoppers production +150%", Cost="1$", Order = 16},
-        new Upgrade {Name = "Experimenting phase", Description = "Before you reveal your new product, you must test it to see if it holds up to the expectations. Build a machine to shoot them into a pool of water to test how long they last.", Boost= "Gobstoppers production +50%", Cost="1$", Order = 17},
-        new Upgrade {Name = "24h Gobstoppers!", Description = "Your Gobstoppers surpass expectations! Each one lasts a total of 24h in water. Is it possible to create a Gobstopper that lasts forever?", Boost= "Sales +50%", Cost="1$", Order = 18},
-        new Upgrade {Name = "Everlasting Gobstoppers", Description = "These new Gobstoppers are meant for children who are given very little allowance money. You can suck on them all year and they will never get any smaller.", Boost= "Sales +500%", Cost="1$", Order = 19},
+        new Upgrade {Name = "Gobstoppers", Description = "You need more variety of sweets to appeal to more people. How about something that lasts in your mouth but you do not need to chew?", Boost= "New factory sector", Cost="30000$", Order = 13},
+        new Upgrade {Name = "New flavors", Description = "You create new flavors of Godstoppers", Boost= "Sales +50%", Cost="800$", Order = 14},
+        new Upgrade {Name = "A variety of flavors, One Gobstopper", Description = "What if you combine all of the flavors created into one single gobstopper?", Boost= "Gobstopper Production +50%", Cost="800$", Order = 15},
+        new Upgrade {Name = "New recipe - Longer lasting", Description = "People like your Gobstoppers so much they are asking for longer lasting ones! Create a new recipe for them to last longer.", Boost= "Gobstoppers production +150%", Cost="600$", Order = 16},
+        new Upgrade {Name = "Experimenting phase", Description = "Before you reveal your new product, you must test it to see if it holds up to the expectations. Build a machine to shoot them into a pool of water to test how long they last.", Boost= "Gobstoppers production +50%", Cost="1000$", Order = 17},
+        new Upgrade {Name = "24h Gobstoppers!", Description = "Your Gobstoppers surpass expectations! Each one lasts a total of 24h in water. Is it possible to create a Gobstopper that lasts forever?", Boost= "Sales +50%", Cost="4000$", Order = 18},
+        new Upgrade {Name = "Everlasting Gobstoppers", Description = "These new Gobstoppers are meant for children who are given very little allowance money. You can suck on them all year and they will never get any smaller.", Boost= "Sales +500%", Cost="9000$", Order = 19},
 
 
         //Oompa Loompa upgrades
-        new Upgrade {Name = "Oompa Loompa Wellness program", Description = "Implement a wellness program to ensure the health and happiness of your workers", Boost= "Production Speed +50%", Cost="1$", Order = 20},
-        new Upgrade {Name = "Oompa Loompa Eficiency Training", Description = "Boost production speed with better-trained Oompa Loompas", Boost= "Production Speed +50%", Cost="1$", Order = 21},
-        new Upgrade {Name = "Oompa Loompa Retirement plan", Description = "Implement a retirement plan for aging Oompa Loompas to boost morale", Boost= "Production Speed +50%", Cost="1$", Order = 22},
+        new Upgrade {Name = "Oompa Loompa Wellness program", Description = "Implement a wellness program to ensure the health and happiness of your workers", Boost= "Production Speed +50%", Cost="10000$", Order = 20},
+        new Upgrade {Name = "Oompa Loompa Eficiency Training", Description = "Boost production speed with better-trained Oompa Loompas", Boost= "Production Speed +50%", Cost="1500$", Order = 21},
+        new Upgrade {Name = "Oompa Loompa Retirement plan", Description = "Implement a retirement plan for aging Oompa Loompas to boost morale", Boost= "Production Speed +50%", Cost="20000$", Order = 22},
         
 
         new Upgrade {Name = "", Description = "", Boost= "", Cost="", Order = 100}
